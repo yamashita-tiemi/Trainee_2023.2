385aa19c2ab6df395b2ea64817865d6a13f6cae4
liviaguirao <farialivia@gmail.com>
2023-10-11 22:51:15 -0300

    landing page com ajustes finalizados

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -6,29 +6,62 @@ const arrowBtns = document.querySelectorAll(".wrapper i");
 
 let isDragging = false, startX, startScrollLeft;
 
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowBtns[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowBtns[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+    
+}
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+
 const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
     startX = e.pageX;
     startScrollLeft = carousel.scrollLeft;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
     if (!isDragging) return;
     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
 }
 
 const dragStop = () => {
     isDragging = false;
     carousel.classList.remove("dragging");
+    showHideIcons();
 }
 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+       setTimeout(() => showHideIcons());
     });
 });
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
+
+
