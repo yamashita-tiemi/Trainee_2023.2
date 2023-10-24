69af2b20f911753cdeb3fe0949cf2324e961abba
liviaguirao <farialivia@gmail.com>
2023-10-24 00:22:29 -0300

    colocando olhinho no input de senha

--- public/js/userList.js
+++ public/js/userList.js
@@ -30,3 +30,28 @@ function confirmarSalvarFormulario() {
         closeModal('createModal');
     }
 }
+
+function mostrarSenha(inputId) {
+    var inputPass = document.getElementById(inputId);
+    var btnShowPass = document.getElementById(`btn-senha-${inputId}`);
+
+    if (inputPass.type == "password") {
+        inputPass.setAttribute("type", "text");
+        btnShowPass.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+    } else {
+        inputPass.setAttribute("type", "password");
+        btnShowPass.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
+    }
+}
+
+function inputChanged(inputId) {
+    var input = document.getElementById(inputId);
+    var btnShowPass = document.getElementById(`btn-senha-${inputId}`);
+
+    if (input.value) {
+        btnShowPass.style.display = "block";
+        
+    } else {
+        btnShowPass.style.display = "none";
+    }
+}
