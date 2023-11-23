4c861c84e945c69a0f50259ba9381812cd266f4c
TaynaraFerraz <taynaracferraz@gmail.com>
2023-11-23 17:37:49 -0300

    teste na rota da paginacao da lista de usuarios

--- app/Controllers/PaginacaoController.php
+++ app/Controllers/PaginacaoController.php
@@ -5,32 +5,36 @@ namespace App\Controllers;
 use App\Core\App;
 use Exception;
 
-class UsuariosController{
+class PaginationController{
 
     public function view(){
         if(isset($_GET['pagina'])){
             $page = intval($_GET['pagina']);
+            if($page<=0){
+                return redirect ('posts');
+            }
+        } else {
+            $page = 1;
         }
 
-        if($page<=0){
-            return redirect ('site/listas-de-posts');
-        }
+        $users = App::get('database')->selectAll('users');
+        // $posts = App::get('database')->selectAll('posts');
+        // $tables = [
+        //     'posts'=> $posts,
+        // ];
 
-        $posts = App::get('database')->selectAll('posts');
-        $tables = [
-            'posts'=> $posts,
-        ];
-
-        $itens_por_pag = 6;
+        $itens_por_pag = 10;
         $start_limit = $itens_por_pag * $page -  $itens_por_pag;
-        $roust_count = App::get('database')->CountAll($tables);
+        $roust_count = App::get('database')->countAll('posts');
 
-        if($start_limit> $roust_count){
+        if($start_limit > $roust_count){
             return redirect ('admin/userListAdm');
         }
 
-        $total_page = ceil( $roust_count/ $itens_por_pag);
+        $posts = App::get('database')->pagination('posts', $start_limit, $itens_por_pag);
 
-        return view ('site/listas-de-posts',compact( 'users', 'page', 'total_page') );
+        $total_page = ceil($roust_count/ $itens_por_pag);
+
+        return view('site/listas-de-posts', compact('users', 'posts', 'page', 'total_page') );
     }
 }
--- app/views/Include/IncludePaginacao.php
+++ app/views/Include/IncludePaginacao.php
@@ -3,25 +3,27 @@
   <ul class="pagination">
 
     <li class="page-item">
-      <a class="page-link text-dark" href="?pagina <? $page > 1 ? $page - 1 : 1 ?>"  aria-label="Anterior">
+      <a class="page-link text-dark" href="?pagina= <?= $page > 1 ? $page - 1 : 1 ?>"  aria-label="Anterior">
         <span aria-hidden="true">&laquo;</span>
       </a>
     </li>
 
     <?php for($page_number = 1; $page_number <= $total_page; $page_number++) : ?>
 
-        <li class="page-item <?php $page_number == $page ? "active" : "" ?>">
-            <a href="?pagina <?= $page_number ?>" class="page-link text-dark">
+        <li class="page-item <?= $page_number == $page ? "active" : "" ?>">
+            <a href="?pagina= <?= $page_number ?>" class="page-link text-dark">
                 <?= $page_number ?>
             </a>
         </li>
 
     <?php endfor; ?>
 
+
     <li class="page-item">
-      <a class="page-link text-dark" href="?pagina <? $page < $total_page ? $page + 1 :$total_page ?>" aria-label="Próximo">
-        <span aria-hidden="true">&laquo;</span>
+      <a class="page-link text-dark" href="?pagina= <?= $page < $total_page ? $page + 1 : $total_page ?>" aria-label="Próximo">
+        <span aria-hidden="true">&raquo;</span>
       </a>
     </li>
+
   </ul>
 </nav>
