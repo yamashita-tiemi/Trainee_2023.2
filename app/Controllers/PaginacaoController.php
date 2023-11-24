c8960604fd4e8ed4e4691052e73d54d6844bf5f2
AureaStar <aurea.starlight@gmail.com>
2023-11-23 21:15:21 -0300

    Eu alterei a função view da paginação pra ela funcionar pra qualquer  tabela que você quiser entrar.  Mudei o nome das listas pra ficar certinho igual ao nome da tabela.

--- app/Controllers/PaginacaoController.php
+++ app/Controllers/PaginacaoController.php
@@ -8,33 +8,36 @@ use Exception;
 class PaginationController{
 
     public function view(){
+        $url = $_SERVER['REQUEST_URI'];
+        $url_parts = explode('/', $url);
+        $table = end($url_parts);
         if(isset($_GET['pagina'])){
             $page = intval($_GET['pagina']);
             if($page<=0){
-                return redirect ('users');
+                return redirect ($table);
             }
         } else {
             $page = 1;
         }
 
         $users = App::get('database')->selectAll('users');
-        // $posts = App::get('database')->selectAll('posts');
+        $posts = App::get('database')->selectAll('posts');
         // $tables = [
         //     'posts'=> $posts,
         // ];
 
         $qntd_users = 5;
         $start_limit = $qntd_users * $page -  $qntd_users;
-        $roust_count = App::get('database')->countAll('users');
+        $roust_count = App::get('database')->countAll($table);
 
         if($start_limit > $roust_count){
-            return redirect ('admin/users');
+            return redirect ("$table");
         }
 
-        $users = App::get('database')->pagination('posts', $start_limit, $qntd_users);
+        $users = App::get('database')->pagination($table, $start_limit, $qntd_users);
 
         $total_page = ceil($roust_count/ $qntd_users);
 
-        return view('admin/users', compact('users', 'posts', 'page', 'total_page') );
+        return view("admin/$table" . "ListAdm", compact('users', 'posts', 'page', 'total_page') );
     }
 }
