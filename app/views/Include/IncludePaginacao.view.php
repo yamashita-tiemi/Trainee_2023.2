83ab057a61c73fab10a18e4a879bacc700ec4691
TaynaraFerraz <taynaracferraz@gmail.com>
2023-11-27 22:35:46 -0300

    testando responsividade da lista de usuarios

--- app/views/Include/IncludePaginacao.view.php
+++ app/views/Include/IncludePaginacao.view.php
@@ -1,31 +1,32 @@
 <link rel="stylesheet" href="../../../public/css/paginacao.css">
+<div class="pagination-div">
+  <nav aria-label="Navegação da página" class="wt-3">
 
-<nav aria-label="Navegação da página" class="wt-3">
+    <ul class="pagination">
 
-  <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link text-dark" href="?pagina= <?= $page > 1 ? $page - 1 : 1 ?>"  aria-label="Anterior">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
 
-    <li class="page-item">
-      <a class="page-link text-dark" href="?pagina= <?= $page > 1 ? $page - 1 : 1 ?>"  aria-label="Anterior">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
+      <?php for($page_number = 1; $page_number <= $total_page; $page_number++) : ?>
 
-    <?php for($page_number = 1; $page_number <= $total_page; $page_number++) : ?>
+          <li class="page-item <?= $page_number == $page ? "active" : "" ?>">
+              <a href="?pagina= <?= $page_number ?>" class="page-link text-dark">
+                  <?= $page_number ?>
+              </a>
+          </li>
 
-        <li class="page-item <?= $page_number == $page ? "active" : "" ?>">
-            <a href="?pagina= <?= $page_number ?>" class="page-link text-dark">
-                <?= $page_number ?>
-            </a>
-        </li>
-
-    <?php endfor; ?>
+      <?php endfor; ?>
 
 
-    <li class="page-item">
-      <a class="page-link text-dark" href="?pagina= <?= $page < $total_page ? $page + 1 : $total_page ?>" aria-label="Próximo">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
+      <li class="page-item">
+        <a class="page-link text-dark" href="?pagina= <?= $page < $total_page ? $page + 1 : $total_page ?>" aria-label="Próximo">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
 
-  </ul>
-</nav>
+    </ul>
+  </nav>
+</div>
